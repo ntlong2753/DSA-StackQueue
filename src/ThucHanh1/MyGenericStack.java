94e1c44bf503ddd95172c08fe8e7cca141bae9bf
nguyenthanhlongvn03 <nguyenthanhlongvn03@gmail.com>
2026-01-06 10:06:02 +0700

    add

--- src/ThucHanh1/MyGenericStack.java
+++ src/ThucHanh1/MyGenericStack.java
@@ -3,66 +3,38 @@ package ThucHanh1;
 import java.util.EmptyStackException;
 import java.util.LinkedList;
 
-public class MyGenericStack <T> {
-    private LinkedList <T> stack;
+import static sun.util.locale.LocaleUtils.isEmpty;
+
+public class MyGenericStack<T> {
+    private LinkedList<T> stack;
 
     public MyGenericStack() {
-        stack = new LinkedList();
+        stack = new LinkedList<>();
     }
 
-    public void push(T element){
+    // Cài đặt phương thức push()
+    public void push(T element) {
         stack.addFirst(element);
     }
 
-    public T pop(){
-        if(stack.isEmpty()){
+    // Cài đặt phương thức pop()
+    public T pop() {
+        if (isEmpty()) {
             throw new EmptyStackException();
         }
         return stack.removeFirst();
     }
 
+    // Cài đặt phương thức size()
     public int size() {
         return stack.size();
     }
 
+    // Cài đặt phương thức isEmpty()
     public boolean isEmpty() {
         if (stack.size() == 0) {
             return true;
         }
-        else return false;
-    }
-
-    static void stackOfStrings() {
-        MyGenericStack<String> stack = new MyGenericStack();
-        stack.push("five");
-        stack.push("Four");
-        stack.push("Three");
-        stack.push("Two");
-        stack.push("One");
-
-        System.out.println("1.1 Size of stack after push operation: " + stack.size());
-        System.out.println("1.2. Pop elements from stack: ");
-
-        while (!stack.isEmpty()) {
-            System.out.println(" %s, " + stack.pop());
-        }
-        System.out.println("\n1.3. Size of stack after pop operation: " + stack.size());
-    }
-
-    static void stackOfIntegers() {
-        MyGenericStack<Integer> stack = new MyGenericStack();
-        stack.push(5);
-        stack.push(4);
-        stack.push(3);
-        stack.push(2);
-        stack.push(1);
-
-        System.out.println("2.1. Size of stack after push operation: " + stack.size());
-        System.out.println("2.2. Pop elements from stack: ");
-
-        while (!stack.isEmpty()) {
-            System.out.println("%d, " + stack.pop());
-        }
-        System.out.println("\n3.3. Size of stack after pop operation: " + stack.size());
+        return false;
     }
 }
